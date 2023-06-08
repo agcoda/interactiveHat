fc23ca21d77419b7197456ae8929ca04d2f490cc
agcoda <agcoda@gmail.com>
2023-06-08 14:06:23 -0700

    Update hatCommander.py
    added serial function and output

--- hatCommander.py
+++ hatCommander.py
@@ -1,46 +1,74 @@
+#!/user/bin/env python
+
 #Function: 
 #   Take input from handheld controller
 #   Pass input to next module
 
+######################################
 #Unit 1: Take input from handheld controller
 #Logitech 310 PC Controller
 #Controller has
 #   omni D pad, 2 joysticks, XYAB buttons, RL trigger, RL bumper
 
-#sys for exit
+#sys for exit, time for general sleep
 import sys
+import time
+
+#will write to arduino over serial
+import serial
 
 #bring in all scancodes for buttons on desired controller
 from logitech310 import *
 
+#initialize the serial communication
+ser = serial.Serial(
+    port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+    baudrate = 9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1
+)
 
 #get the inputs from that controlfile
 from evdev import InputDevice, categorize, ecodes, KeyEvent
 gamepad = InputDevice('/dev/input/'+CONTROLFILE)
-#just look for the scancodes
+#just look for the scancodes in ctrlr file and save which button was pressed
+btn_pressed = 'none'
+#this is the main loop we stay in until home button is pressed
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
         keyevent = categorize(event)
+        #raspi currently running 3.9, cant use match, case yet
         if keyevent.keystate == KeyEvent.key_down:
             if keyevent.scancode == btn_b:
-                print('B')
+                btn_pressed = 'B'
             if keyevent.scancode == btn_a:
-                print('A')
+                btn_pressed = 'A'
             if keyevent.scancode == btn_x:
-                print('X')
+                btn_pressed = 'X' 
             if keyevent.scancode == btn_y:
-                print('Y')    
+                btn_pressed = 'Y'     
             if keyevent.scancode == btn_tl:
-                print('TL')
+                btn_pressed = 'TL' 
             if keyevent.scancode == btn_tr:
-                print('TR')
+                btn_pressed = 'TR' 
             if keyevent.scancode == btn_select:
-                print('select')
+                btn_pressed = 'select' 
             if keyevent.scancode == btn_start:
-                print('start')    
+                btn_pressed = 'start'     
             if keyevent.scancode == btn_home:
-                sys.exit("Home button closes program")
+                btn_pressed = 'home'
             if keyevent.scancode == btn_l3:
-                print('L3')
+                btn_pressed = 'L3' 
             if keyevent.scancode == btn_r3:
-                print('R3')
+                btn_pressed = 'R3' 
+    if btn_pressed == 'home':
+        sys.exit("Home button closes program")
+
+    ##################################
+    #Unit 2: output 
+    #once a button is pushed we need to pack and send it to the next device
+
+    ser.write(btn_pressed)
+
