29963d42e4b9a5cf38e3dc26d29e4427e9458216
agcoda <agcoda@gmail.com>
2023-06-08 13:34:14 -0700

    var xfer
    xfer logitech vars to external file

--- hatCommander.py
+++ hatCommander.py
@@ -10,20 +10,22 @@
 #sys for exit
 import sys
 
+import logitech310
+
 #Get input from device
 #define which event corresponds to the controller
-CONTROLFILE = 'event1'
-btn_a = 304
-btn_b = 305
-btn_x = 307
-btn_y = 308
-btn_tl = 310
-btn_tr = 311
-btn_select = 314
-btn_start = 315
-btn_home = 316
-btn_l3 = 317
-btn_r3 = 318
+#CONTROLFILE = 'event1'
+#btn_a = 304
+#btn_b = 305
+#btn_x = 307
+#btn_y = 308
+#btn_tl = 310
+#btn_tr = 311
+#btn_select = 314
+#btn_start = 315
+#btn_home = 316
+#btn_l3 = 317
+#btn_r3 = 318
 
 
 
