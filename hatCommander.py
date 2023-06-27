9179b88e9a7caac24ce7b01c0ac43ed8c46012c8
agcoda <agcoda@gmail.com>
2023-06-27 11:26:38 -0700

    refactor
    oo rewrite cont

--- hatCommander.py
+++ hatCommander.py
@@ -42,17 +42,16 @@ btn_pressed = 'none'
 
 def main():
 
-    serialComm = SerialComm()
+    serialComm = SerialComm() #this init will start serial comms
 
     action = CtrlrAction()
-
-    serialComm.initSerial()
 
     #main loop provided by evdev
     for event in gamepad.read_loop():
         if event.type == evdev.ecodes.EV_KEY:
             keyevent = evdev.categorize(event)
-            btn_pressed = action.btnInt(keyevent)
+            if keyevent.keystate == evdev.KeyEvent.key_down:
+                btn_pressed = action.btnInt(keyevent)
 
     
             
@@ -63,43 +62,34 @@ def main():
 
 
 
-    #get response for integrity, if one sent
-    while ser.in_waiting:
-        action = ser.readline()
-        #action = action.decode("utf-8")
-        #print("received:" +action +"\n")
-        print(action)
-        #clear out the rest of the buffer
-        while ser.in_waiting:
-            trash = ser.readline()
+
 
 class CtrlrAction:
     #handles button presses that are just off or on, like AB etc
     def btnInt(keyevent):
         #raspi currently running 3.9, cant use match, case yet
-        if keyevent.keystate == evdev.KeyEvent.key_down:
-            if keyevent.scancode == btn_b:
-                btn_pressed = 'B'
-            if keyevent.scancode == btn_a:
-                btn_pressed = 'A'
-            if keyevent.scancode == btn_x:
-                btn_pressed = 'X' 
-            if keyevent.scancode == btn_y:
-                btn_pressed = 'Y'     
-            if keyevent.scancode == btn_tl:
-                btn_pressed = 'L' 
-            if keyevent.scancode == btn_tr:
-                btn_pressed = 'R' 
-            if keyevent.scancode == btn_select:
-                btn_pressed = 'C' 
-            if keyevent.scancode == btn_start:
-                btn_pressed = 'S'     
-            if keyevent.scancode == btn_home:
-                btn_pressed = 'H'
-            if keyevent.scancode == btn_l3:
-                btn_pressed = 'Q' 
-            if keyevent.scancode == btn_r3:
-                btn_pressed = 'W' 
+        if keyevent.scancode == btn_b:
+            btn_pressed = 'B'
+        if keyevent.scancode == btn_a:
+            btn_pressed = 'A'
+        if keyevent.scancode == btn_x:
+            btn_pressed = 'X' 
+        if keyevent.scancode == btn_y:
+            btn_pressed = 'Y'     
+        if keyevent.scancode == btn_tl:
+            btn_pressed = 'L' 
+        if keyevent.scancode == btn_tr:
+            btn_pressed = 'R' 
+        if keyevent.scancode == btn_select:
+            btn_pressed = 'C' 
+        if keyevent.scancode == btn_start:
+            btn_pressed = 'S'     
+        if keyevent.scancode == btn_home:
+            btn_pressed = 'H'
+        if keyevent.scancode == btn_l3:
+            btn_pressed = 'Q' 
+        if keyevent.scancode == btn_r3:
+            btn_pressed = 'W' 
         print(btn_pressed)
         if btn_pressed == 'H':
             sys.exit("Home button closes program")
@@ -110,21 +100,35 @@ class CtrlrAction:
         if keyevent.keystate == evdev.KeyEvent.key_down:
             btnPressed = keyevent.scancode
             print(btnPressed)
+        return btn_pressed
 
 class SerialComm():
-    ser = serial.Serial(
-        port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
-        baudrate = 9600,
-        parity=serial.PARITY_NONE,
-        stopbits=serial.STOPBITS_ONE,
-        bytesize=serial.EIGHTBITS,
-        timeout=1
-    )
+    def __init__(self):
+        
+        self.ser = serial.Serial(
+            port='/dev/ttyS0', #Replace ttyS0 with ttyAM0 for Pi1,Pi2,Pi0
+            baudrate = 9600,
+            parity=serial.PARITY_NONE,
+            stopbits=serial.STOPBITS_ONE,
+            bytesize=serial.EIGHTBITS,
+            timeout=1
+        )
 
-    def writeToArd():    
+    def writeToArd(self):    
         #pyserial can't take str,encode first to utf8 or ascii
-        ser.flush
-        ser.write(btn_pressed.encode("utf-8"))
-        ser.flush
+        self.ser.flush
+        self.ser.write(btn_pressed.encode("utf-8"))
+        self.ser.flush
+
+    def readFromArd(self):
+        #get response for integrity, if one sent
+        while self.ser.in_waiting:
+            action = self.ser.readline()
+            #action = action.decode("utf-8")
+            #print("received:" +action +"\n")
+            print(action)
+            #clear out the rest of the buffer
+            while self.ser.in_waiting:
+                trash = self.ser.readline()
 
 main()
--- logitech310.py
+++ logitech310.py
@@ -14,3 +14,18 @@ btn_start = 315
 btn_home = 316
 btn_l3 = 317
 btn_r3 = 318
+
+#event.codes for joysticks and triggers
+stick_l_x = 0
+stick_l_y = 1
+stick_r_x = 3
+stick_r_y = 4
+trig_l = 2
+trig_r = 5
+
+#three attributes to events type, code, value
+#type 1 =btn, type 3=joystick/trigger
+#for btns value =1 for press, 0 for release
+
+#for joysticks/triggers vals go from-32768 to 32767
+#for joysticks the code is 0-5 but it also sends a full zero code btwn each
