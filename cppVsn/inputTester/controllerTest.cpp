dfc36594bcc355fe93d3a76e2e8e23fdbfc61bbc
agcoda <agcoda@gmail.com>
2023-08-20 15:21:25 -0700

    Update controllerTest.cpp
    more work on input tester classes. Added methods for input storage and sending

--- cppVsn/inputTester/controllerTest.cpp
+++ cppVsn/inputTester/controllerTest.cpp
@@ -37,17 +37,31 @@ range actions repeat until a change or 0 is received.
 //more advanced may have touch and motion, but we dont need that here
 class Ctrlr{
 	protected:
+		bool new_pressed = 1;
+		int pressed_i = 0;
+		//{type, code, value}
+		int pressed_trip[3] = {0,0,0};
 		
 		std::string pressed_name = "n";
 		char pressed_type = 'n';
 		char pressed_dir = 'n';
-		int last_pressed_i = 0;
-		std::vector<int> btn_codes;
+
+		int num_btns = 11;
 
 		//standard list of buttons most controllers have
 		std::vector<std::string> btn_names{ "a", "b", "x", 
 		"y", "tl", "tr", "select", "start", "home", 
 		"l3", "r3"};
+
+		//each controller will need to overwrite these with the vals it sends for them
+		std::vector<int> btn_codes{0,0,0,
+		0,0,0,0,0,0,
+		0,0};
+
+		//for joysticks/triggers vals go from-32768 to 32767
+		//for joysticks the code is 0-5 but it also sends a full zero code btwn each
+		//each ctrlr needs update deadzone on const
+		int deadzone = 0; //they don't quite reset to zero so only send the val if its outside this range
 
 
 	public:
@@ -76,27 +90,90 @@ class Ctrlr{
 			}
 		}
 
+		void setRngDir(int value){
+			//only want to update if its + or - and outside the deadzone
+			//u(p) for positive dir, d(own) for neg
+			if(value > deadzone){
+				pressed_dir = 'u';
+			}
+			else if (value < -deadzone){
+				pressed_dir = 'd';
+			}
+			else {
+				//getting here means its a repeated value
+				//which shoulve been ignored
+			}
+
+		}
+
+		void setPressedName(int code){
+			pressed_name = std::to_string(code);
+			pressed_trip[1] = code;
+		}
+
 		void lastPressedUpdate(int i){
-			last_pressed_i = i;
+			pressed_i = i;
 		}
 
-		//when a button is pressed call this to update all info on that
-		void buttonPressed(){
+		//requires caller to understand or figure out whats in each location
+		int getPressedIndex(){
+			return pressed_i;
+		}
+
+		//takes the triplet type,code,value and stores it as a pressed button
+		void updatePressed(int type, int code, int value){
+			//first check if this is a repeated press
+			if(code != pressed_trip[1]){
+				new_pressed = true;
+				//if its a new button, update pressed
+
+				pressed_trip[0] = type;
+				pressed_trip[1] = code;
+				pressed_trip[2] = value;
+
+				setType(type);
+				setPressedName(code);
+				//maybe overload this with bool? worth?
+				if(pressed_type == 'b'){
+					setBtnDir(value);
+				}
+				else if(pressed_type == 'r'){
+					setRngDir(value);
+				}
+				else {
+					//unknown type found, log it
+				}
+			}
+			else{
+				//if its a repeated button ignore it
+				new_pressed = false;
+				//unless if its a repeated range and dir is different update
+				if(type == 'r' && ((value>0) != (pressed_trip[2]>0))){
+					setRngDir(value);
+					new_pressed = true;
+				}
+			}
+
+
+
 
 		}
+
+
 
 
 };
 
-class Logitech310 : Ctrlr {
+class Logitech310 : public Ctrlr {
 	private:
 		std::string nameStr = "Logitech310";
-
+		
+		/*	
 		//specific codes for the Logitech310 gotten from testing
 		std::vector<int> btn_codes{304, 305, 307,
 			308, 310, 311, 314, 315, 316, 
-			317, 318};
-/*		// replaced direct definitions with vectors for searching
+			317, 318};	
+		//replaced direct definitions with vectors for searching
 		int btn_a = 304;
 		int btn_b = 305;
 		int btn_x = 307;
@@ -108,15 +185,18 @@ class Logitech310 : Ctrlr {
 		int btn_home = 316;
 		int btn_l3 = 317;
 		int btn_r3 = 318;
-*/		
-		int last_pressed = 0; //index of last pressed
-
-		//for joysticks/triggers vals go from-32768 to 32767
-		//for joysticks the code is 0-5 but it also sends a full zero code btwn each
-		int DEADZONE = 150; //they don't quite reset to zero so only send the val if its outside this range
-		Logitech310
+		*/
+		
 	
 	public:
+
+		Logitech310(){
+			deadzone = 150;
+			btn_codes = {304, 305, 307,
+			308, 310, 311, 314, 315, 316, 
+			317, 318};
+		}
+
 		std::string getName(){
 			return nameStr;
 		}
@@ -155,10 +235,33 @@ class Serial : Communicator{
 		std::vector<char> output_chars{'A', 'B', 'X',
 			'Y', 'L', 'R', 'S', 'T', 'H',
 			'Q', 'W'};
+		//type, direction, null
+		char msg_out[3] = {'N','N','N'};
 	
 	public:
+		//assign the index of the action in ctrlr to one of the available chars in serial
+		
+		/********
+		requires output_chars to be at least the same size
+		change to take button names and translate them instead
+		should have this method just find the closest matching serial char
+		*********/
 		char translate_to_serial(Logitech310 &logitech){
-			*logitech.
+			
+			msg_out[0] = output_chars[logitech.getPressedIndex()];
+		}
+
+		//sends a triplet of type, dir, null 
+		void sendMsg(){
+			//using cout for testing in diff environ
+			for(int i=0;i<3;i++){
+				std::cout << msg_out[i];
+				//send the serial bit
+			}
+		}
+
+		char getMsg(){
+
 		}
 			 
 };
@@ -167,7 +270,20 @@ class Serial : Communicator{
 
 //test driver
 int main(){
+	int test_type, test_code, test_value = 0;
 	Logitech310 ctrlr1;
+	Serial comm1;
+
+	//driver
+	//emulate a pushed button A (type, code, value)
+	test_type = 1;
+	test_code = 304;
+	test_value = 1;
+	//when button pushed update
+	ctrlr1.updatePressed(test_type, test_code, test_value);
+
+	//check if the ctrlr foudn this to be a new button, if so, send it
+	if(ctrlr1.)
 	std::cout << ctrlr1.getName();
 
 	return 0;
